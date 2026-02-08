45d748dbd2acb601a73b8f8bd0d9ee811ee3fbad
gersin <gersonfernandesribeiro48@gmail.com>
2026-02-08 16:00:51 -0300

    fix: recursos para os contatos

---
+++ src/main/java/com/java/boilerplate/controller/ChatContacts/ChatContactsController.java
@@ -0,0 +1,37 @@
+package com.java.boilerplate.controller.ChatContacts;
+
+import com.java.boilerplate.model.ChatContacts;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ChatContactsController {
+    private final ChatContactsHandler chatContactsHandler;
+
+    public ChatContactsController(ChatContactsHandler chatContactsHandler) {
+        this.chatContactsHandler = chatContactsHandler;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChatContacts>> getChatContacts(
+            @RequestParam String username,
+            @RequestParam Long nextEntry,
+            @RequestParam(required = false, defaultValue = "20") int limit
+    ) { return chatContactsHandler.getChatContacts(username, nextEntry, limit); }
+
+    @PostMapping
+    public ResponseEntity<ChatContacts> updateContactStatus(
+            @RequestParam Long receiverId,
+            @RequestParam Long senderId,
+            @RequestParam(required = false, defaultValue = "false") Boolean isBlocked
+    ) { return chatContactsHandler.updateContactStatus(receiverId, senderId, isBlocked); }
+
+    @DeleteMapping
+    public ResponseEntity<String> removeContact(
+            @RequestParam Long userId,
+            @RequestParam Long contactId
+    ) { return chatContactsHandler.removeContact(userId, contactId); }
+}
--- src/main/java/com/java/boilerplate/repository/IChatContactsRepository.java
+++ src/main/java/com/java/boilerplate/repository/IChatContactsRepository.java
@@ -1,10 +1,21 @@
 package com.java.boilerplate.repository;
 
 import com.java.boilerplate.model.ChatContacts;
-import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IChatContactsRepository extends IBaseRepository<ChatContacts> {
     Boolean checkBlockedContact(Long receiverId, Long senderId);
+    Optional<ChatContacts> findByUser_IdUserAndContact_IdUser(Long userId, Long contactId);
+    void deleteByUser_IdUserAndContact_IdUser(Long userId, Long contactId);
+    List<ChatContacts> findContactsByUsername(
+            @Param("username") String username,
+            @Param("nextEntryId") Long nextEntryId,
+            Pageable pageable
+    );
 }
